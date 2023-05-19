376ddbed909c9d0039c252911bad8025413248f0
lixin <1453287107@qq.com>
2023-05-19 17:05:29 +0800

    init(project): 初始化项目

---
+++ README.md
@@ -0,0 +1,33 @@
+# uniapp-vite-vue3 项目模板
+
+### 开发环境
+
+1. node v16.17.0 (建议使用nvm管理node版本，nvm不会的自行百度，windows需要使用管理员权限打开终端执行nvm的命令) [下载windows安装包](https://github.com/coreybutler/nvm-windows/releases)
+2. npm  v9.6.5
+3. pnpm v8.3.1
+
+### 依赖安装
+
+```shell
+npm i -g pnpm 
+pnpm i
+```
+#### 注意
+1. 本项目使用淘宝源
+2. ~~pinia 使用暂时有问题 待修复~~
+
+### 开发环境运行
+
+| 命令          | 说明     |
+|-------------|--------|
+| pnpm dev:h5 | h5端    |
+| pnpm dev:mp-weixin   | 微信小程序端 |
+
+
+### 打包生产环境
+
+
+| 命令          | 说明     |
+|-------------|--------|
+| pnpm build:h5 | h5端    |
+| pnpm build:mp-weixin   | 微信小程序端 |
---
+++ src/pages/home/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <view class="content">
+    <view style="padding: 20px">
+      <u-tag :text="state.tag" plain size="mini" type="warning"></u-tag>
+      <u-alert
+        :title="state.title"
+        type="warning"
+        effect="dark"
+        :description="state.description"
+      ></u-alert>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+  import { reactive } from 'vue';
+  const state = reactive({
+    title: 'title',
+    tag: '测试标签',
+    description: 'description',
+  });
+</script>
+
+<style>
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .logo {
+    height: 200rpx;
+    width: 200rpx;
+    margin-top: 200rpx;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 50rpx;
+  }
+
+  .text-area {
+    display: flex;
+    justify-content: center;
+  }
+
+  .title {
+    font-size: 36rpx;
+    color: #8f8f94;
+  }
+</style>
